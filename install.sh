11cfcab2b2337aa35215edd508688fed96a47eb6
Brad <git@bradm.io>
2023-04-28 17:41:16 +0930

    Update install.sh
    Some kernels need RDPMC to be enabled in /sys/devices/cpu/rdpmc. I have added this and for those kernels where it doesn't exist, the error output (for the file not being found) is just discarded.

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #insert the module
 sudo modprobe msr
 sudo insmod user_rdpmc.ko
+
+#Enable rdpmc in kernel according to:
+#https://github.com/HewlettPackard/quartz/blob/master/README.md
+echo 2 | sudo tee /sys/devices/cpu/rdpmc 2> /dev/null
